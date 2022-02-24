1c6dcd918ae28ed5f532644a821344b1ee1ab7c1
Matthew Manley <MatthewManley@users.noreply.github.com>
2022-02-24 16:20:42 -0500

    Added caching, defering, and responding since apparently fetching guilds can take a bit

--- ActivityBot/Commands/StatsCommand.cs
+++ ActivityBot/Commands/StatsCommand.cs
@@ -11,15 +11,26 @@ namespace ActivityBot.Commands
     public class StatsCommand
     {
         private readonly DiscordSocketClient discordSocketClient;
+        private readonly IMemoryCache memoryCache;
 
-        public StatsCommand(DiscordSocketClient discordSocketClient)
+        public StatsCommand(DiscordSocketClient discordSocketClient, IMemoryCache memoryCache)
         {
             this.discordSocketClient = discordSocketClient;
+            this.memoryCache = memoryCache;
         }
 
         public async Task Interact(SocketSlashCommand slashCommand)
         {
-            await slashCommand.RespondAsync($"Guilds: {discordSocketClient.Guilds.Count}");
+            int count;
+            if (memoryCache.TryGetValue("guildCount", out count))
+            {
+                await slashCommand.RespondAsync($"Guilds: {count}");
+                return;
+            }
+            await slashCommand.DeferAsync();
+            count = discordSocketClient.Guilds.Count;
+            memoryCache.Set("guildCount", count, TimeSpan.FromMinutes(1));
+            await slashCommand.FollowupAsync($"Guilds: {discordSocketClient.Guilds.Count}");
         }
     }
 }
